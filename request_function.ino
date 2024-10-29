27c48fd55b952dcede590870bc98e34cd7f90baf
Trinadia Kurniasari <109284009+trinadiak@users.noreply.github.com>
2024-10-29 15:24:13 +0700

    Update request_function.ino

--- request_function.ino
+++ request_function.ino
@@ -1,9 +1,9 @@
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 
-#define MYPORT_RX 12 
-#define MYPORT_TX 13
+// #define MYPORT_RX 12 
+// #define MYPORT_TX 13
 
-EspSoftwareSerial::UART modbusPort;  // RX, TX pins for Modbus communication
+// EspSoftwareSerial::UART modbusPort;  // RX, TX pins for Modbus communication
 int registerValue = 45;  // Example register value to return
 
 uint16_t calculateCRC(uint8_t *buffer, int length) {
@@ -24,14 +24,17 @@ uint16_t calculateCRC(uint8_t *buffer, int length) {
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Arduino Ready");
   // modbusPort.begin(9600);  // Set baud rate for modbus
-  modbusPort.begin(9600,SWSERIAL_8N1, MYPORT_RX, MYPORT_TX, false);
+  // modbusPort.begin(9600,SWSERIAL_8N1, MYPORT_RX, MYPORT_TX, false);
 }
 
 void loop() {
-  if (modbusPort.available()) {
+  // if (modbusPort.available()) {
+  if (Serial.available()){
     uint8_t request[8];
-    modbusPort.readBytes(request, 8);
+    // modbusPort.readBytes(request, 8);
+    Serial.readBytes(request,8);
 
     // Validate CRC
     uint16_t requestCRC = request[6] | (request[7] << 8);
@@ -43,7 +46,8 @@ void loop() {
       response[5] = crc & 0xFF;
       response[6] = (crc >> 8) & 0xFF;
 
-      modbusPort.write(response, 7);  // Send response back to master
+      // modbusPort.write(response, 7);  // Send response back to master
+      Serial.write(response,7);
     }
   }
 }
